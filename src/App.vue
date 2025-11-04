26a8db38bf138be67fc074c2726138d09686d7aa
lynx <diaamantkia@gmail.com>
2025-11-04 12:17:13 +0300

    Reactive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue'
+const name = ref('World')
+name.value = 'Vue 3'
+
+const reactiveArray = reactive([1, 2, 3])
+reactiveArray.push(4)
+
+const refArray = ref([4, 5, 6])
+refArray.value.push(7)
+
+const reactiveUser = reactive({
+  user: {
+    firstName: 'Alice',
+    age: 30,
+  },
+})
+reactiveUser.user.age += 1
+
+const refUser = ref({
+  user: {
+    firstName: 'Eve',
+    age: 30,
+  },
+})
+refUser.value.user.age += 1
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p class="greeting">Hello, {{ name }}!</p>
 </template>
 
-<style scoped></style>
+<style src="./App.css"></style>
