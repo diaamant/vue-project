3dd81313b9561bcf0685d68a788b7aeeca0caa04
lynx <diaamantkia@gmail.com>
2025-11-04 18:07:15 +0300

    v-on event

---
+++ src/components/SampleBase.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+const name = ref('John')
+const lastName = ref('Snow')
+
+const count = ref(0)
+</script>
+
+<template>
+  <p class="greeting">Hello, {{ name + ' ' + lastName }}!</p>
+
+  <button v-on:click="count++">Add 1</button>
+</template>
+
+<style src="./App.css"></style>
---
+++ src/components/VOnExamples01.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+const name = ref('John')
+const lastName = ref('Snow')
+
+const count = ref(0)
+
+function increaseCount() {
+  count.value++
+}
+
+function decreaseCount(event) {
+  count.value--
+  console.log(event)
+}
+</script>
+
+<template>
+  <p class="greeting">Hello, {{ name + ' ' + lastName }}!</p>
+
+  <button v-on:click="increaseCount">Add 1</button>
+
+  <button @click="decreaseCount">Decrease 1</button>
+
+  <p>Счетчик: {{ count }}</p>
+</template>
+
+<style src="./App.css"></style>
